8423291613f6b991e4c47e55d33a9603fd1d5b34
dward2012 <dward@semquest.com>
2017-04-11 19:20:53 -0600

    Create simple_sqlite_test_code.py

---
+++ simple_sqlite_test_code.py
@@ -0,0 +1,13 @@
+import dbfunctions
+
+dbfunctions.create_article('test1','whatever1')
+dbfunctions.create_article('test2','whatever2')
+dbfunctions.create_article('test3','whatever3')
+dbfunctions.create_article('test4','whatever4')
+dbfunctions.create_article('test5','whatever5')
+
+dbfunctions.cur.execute('SELECT * FROM articles;')
+thingy = dbfunctions.cur.fetchall()
+
+for thing in thingy:
+  print(thing)
