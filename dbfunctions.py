ff1fd8f23f486b98073c3a717d3989eeaf9209c1
kajuberdut <patrick.shechet@gmail.com>
2017-04-11 19:43:58 -0600

    Added Search Function

--- dbfunctions.py
+++ dbfunctions.py
@@ -35,11 +35,28 @@ def update_article(subject, article_text):
   """
   pass
 
-def search_article(subject, strict=False):
-  """
-  This article should return a list of subjects that contain the subject text given.
-  Strict should return only exact match
-  Case?
-  """
-  pass
+def private_get(subject):
+    cur.execute("SELECT article_text FROM articles WHERE subject='{subject}'".format(subject=subject))
+    article_text = cur.fetchone()[0]
+    return article_text
+
+def search_article(subject_text, strict=False):
+    """
+    Takes a subject_text and optionally strict boolean.BaseException
+    Returns a list of tuples containing: subject, body_text_function
+    The body text function can be called to return the body text of the article.
+    Strict flag makes searches require an exact match.
+    """
+    if not strict:
+        subject_text = '%'+subject_text+'%'
+    search_query = """
+    SELECT subject 
+    FROM articles 
+    WHERE LOWER(subject) like '%{subject}%';
+    """.format(subject=subject_text.lower())
+    cur.execute(search_query)
+    results = cur.fetchall()
+    result_list = [[a[0], lambda subj=a[0] : private_get(subject=subj)] for a in results]
+    return result_list
+
 
