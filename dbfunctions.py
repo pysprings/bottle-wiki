6d41dd3f4c657a59d33e9a70e79fb60c85f7898b
Kajuberdut <kajuberdut@users.noreply.github.com>
2017-04-29 21:06:51 -0600

    Updating functions to use new db methods.

--- dbfunctions.py
+++ dbfunctions.py
@@ -4,6 +4,12 @@ from hashlib import md5
 def hash(text):
     return md5(text.encode()).hexdigest()
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 class Wikidb(object):
     """ A class for handling wiki data. """
 
@@ -11,6 +17,7 @@ class Wikidb(object):
         with open('initialize.sql', 'r') as sql:
             self.tablesql = sql.read().split(';')
         self.conn = sqlite3.connect(db_path)
+        self.conn.row_factory = dict_factory
         self.cur = self.conn.cursor()
         for command in self.tablesql:
             self.cur.execute(command)
@@ -23,15 +30,13 @@ class Wikidb(object):
         history_id = hash(str(subject+body))
         self.cur.execute("""INSERT OR REPLACE INTO history (body, history_id)
         VALUES (?,?)""", (body, history_id))
-        self.cur.execute("""INSERT OR REPLACE INTO articles (subject, history_id)
-        VALUES (?,?)""", (subject.lower(), history_id))
         self.cur.execute("""INSERT INTO authorship(article_subject, author_email, history_id)
         VALUES(?, ?, ?)""", (subject.lower(), author_email.lower(), history_id))
         self.conn.commit()
 
     def detail(self, subject):
         """ Needs docstring """
-        sql = """
+        detail_sql = """
         SELECT subject
         , created_on
         , creator_email
@@ -41,8 +46,8 @@ class Wikidb(object):
         FROM v_first_last
         WHERE subject = ?;
         """
-        self.cur.execute(sql, [subject])
-        article_text = self.cur.fetchone()[0]
+        self.cur.execute(detail_sql, [subject])
+        article_text = self.cur.fetchone()
         return article_text
 
     def search(self, subject_text, strict=False):
@@ -54,9 +59,9 @@ class Wikidb(object):
         if not strict:
             subject_text = '%'+subject_text.lower()+'%'
         search_query = """
-        SELECT subject
-        FROM articles
-        WHERE subject like ?;
+        SELECT DISTINCT article_subject
+        FROM authorship
+        WHERE article_subject like ?;
         """
         self.cur.execute(search_query, [subject_text])
         return list(self.cur.fetchall())
