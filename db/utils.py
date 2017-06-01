33af3d6e26e67efd93ff50e7e9b8779f9d109c2b
Kajuberdut <kajuberdut@users.noreply.github.com>
2017-05-31 18:12:18 -0600

    Rebasing to psyprings master

---
+++ db/utils.py
@@ -0,0 +1,30 @@
+from hashlib import md5
+import os.path
+import json
+
+def hashstring(text):
+    return md5(text.encode()).hexdigest()
+
+class Config(object):
+
+        def __init__(self, path='config.json'):
+            self.defaultconfig = {'db_url':'postgresql:///db_name', 'debug':True}
+
+            if not os.path.isfile(path):
+                print("No config.json found, creating default config.")
+                with open(path, 'w') as conf:
+                    conf.write(json.dumps(self.defaultconfig))
+            with open(path, 'r') as conf:
+                self.config = json.loads(conf.read())
+        
+        def getconfig(self, handle):
+                return self.config.get(handle)
+
+if __name__ == "__main__":
+    config = Config()
+    print(config.config)
+    print(config.getconfig('db_url'))
+
+    context = 'something else'
+    x = lambda context:hashstring(context)
+    print(x(context))
