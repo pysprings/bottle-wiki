1f93aa621da15d1b8a111dd7480b715271b73116
Patrick Shechet <kajuberdut@users.noreply.github.com>
2017-06-01 21:53:37 -0600

    switched config file from json to configparser.

--- db/utils.py
+++ db/utils.py
@@ -1,30 +1,25 @@
-from hashlib import md5
 import os.path
-import json
+import configparser
+from hashlib import md5
+from shutil import copyfile
 
 def hashstring(text):
     return md5(text.encode()).hexdigest()
 
-class Config(object):
+class Config(configparser.ConfigParser):
+    """
+    An object for handling reading and if necessary creation of configuration file.
+    """
+    def __init__(self, path='', *args, **kwargs):
+        self.config = configparser.SafeConfigParser()
+        self.path = 'config.cfg'
+        if not os.path.isfile(self.path):
+            copyfile('default.cfg', 'config.cfg')
+        self.config.read(self.path)
 
-        def __init__(self, path='config.json'):
-            self.defaultconfig = {'db_url':'sqlite:///:memory:', 'debug':True}
-
-            if not os.path.isfile(path):
-                print("No config.json found, creating default config.")
-                with open(path, 'w') as conf:
-                    conf.write(json.dumps(self.defaultconfig))
-            with open(path, 'r') as conf:
-                self.config = json.loads(conf.read())
-        
-        def getconfig(self, handle):
-                return self.config.get(handle)
+    def getconfig(self, option, section):
+        return self.config.get(section, option)
 
 if __name__ == "__main__":
     config = Config()
-    print(config.config)
     print(config.getconfig('db_url'))
-
-    context = 'something else'
-    x = lambda context:hashstring(context)
-    print(x(context))
